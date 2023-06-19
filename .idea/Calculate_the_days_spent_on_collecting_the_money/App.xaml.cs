bde7aa640a5f9b117c72eb58fd6e21aa097a43b1
NontagornB <nontagornboayairuksa@gmail.com>
2023-06-19 21:37:46 +0700

    basic program

---
+++ .idea/Calculate_the_days_spent_on_collecting_the_money/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculate_the_days_spent_on_collecting_the_money
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
